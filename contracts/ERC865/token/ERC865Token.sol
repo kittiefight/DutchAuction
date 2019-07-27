940d2ca87b0d23a7b0da2aa1e5f8b1da4ab2523f
Pavel Rubin <pash7ka@gmail.com>
2019-07-27 05:41:59 +0300

    Fix signature cheking to prevent signature malleability issue, as described in https://github.com/OpenZeppelin/openzeppelin-contracts/pull/1622

--- contracts/ERC865/token/ERC865Token.sol
+++ contracts/ERC865/token/ERC865Token.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.10;
 
 import "./ERC865.sol";
+import "../../openzeppelin-solidity/cryptography/ECDSA.sol";
 import "../../openzeppelin-solidity/token/ERC20/StandardToken.sol";
 
 /**
@@ -12,6 +13,7 @@ import "../../openzeppelin-solidity/token/ERC20/StandardToken.sol";
  */
 
 contract ERC865Token is ERC865, StandardToken {
+    using ECDSA for bytes32;
 
     /* Nonces of transfers performed */
     mapping(bytes => bool) signatures;
@@ -334,32 +336,6 @@ contract ERC865Token is ERC865, StandardToken {
      * @param sig bytes signature, the signature is generated using web3.eth.sign()
      */
     function recover(bytes32 hash, bytes memory sig) public pure returns (address) {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        //Check the signature length
-        if (sig.length != 65) {
-            return (address(0));
-        }
-
-        // Divide the signature in r, s and v variables
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-        if (v < 27) {
-            v += 27;
-        }
-
-        // If the version is correct return the signer address
-        if (v != 27 && v != 28) {
-            return (address(0));
-        } else {
-            return ecrecover(hash, v, r, s);
-        }
+        return hash.toEthSignedMessageHash().recover(sig);
     }
 }
